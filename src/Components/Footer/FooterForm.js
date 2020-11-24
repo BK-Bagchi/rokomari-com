e4e8846bfe083f069d563debd78f6adced0a9267
BK-Bagchi <bkbagchi.dipto@gmail.com>
2020-11-25 01:49:35 +0600

    FooterForm section initial markup done

--- src/Components/Footer/FooterForm.js
+++ src/Components/Footer/FooterForm.js
@@ -3,7 +3,20 @@ import React from 'react'
 const FooterForm = () => {
     return (
         <section className="footer-form">
-
+            <main className="container px-4">
+                <div className="row w-100 m-0 d-flex align-items-center justify-content-center">
+                    <div className="col-md-6">
+                        <h3>Subscribe to Our Newsletter</h3>
+                        <p className="text-muted">Don't miss any updates of our new Course and all the astonishing offers.</p>
+                    </div>
+                    <div className="col-md-6">
+                        <form>
+                            <input type="email" placeholder="Email address" className="email-field" />
+                            <input type="submit" value="Subscribe" className="submit-btn" />
+                        </form>
+                    </div>
+                </div>
+            </main>
         </section>
     );
 };
