f582ac3faacf5b95feecfcea8ab1655ef0025e0e
BK-Bagchi <bkbagchi.dipto@gmail.com>
2020-11-25 09:28:42 +0600

    Card section more markup done

--- src/Components/Cards/Cards.js
+++ src/Components/Cards/Cards.js
@@ -4,7 +4,7 @@ import cardInfo from './CardInfo'
 
 const Cards = () => {
     return (
-        <section className="cards container d-flex flex-wrap align-items-center justify-content-center">
+        <section className="cards container d-flex flex-wrap align-items-center justify-content-center py-5">
             {
                 cardInfo.map(card => {
                     const { id, main, text, image } = card
@@ -13,8 +13,8 @@ const Cards = () => {
                             <div className="img-holder d-flex align-items-center justify-content-center">
                                 <img src={image} alt="Card Img" />
                             </div>
-                            <div className="this-card">
-                                <h4>{main}</h4>
+                            <div className="this-card text-center">
+                                <h4 className="mt-2">{main}</h4>
                                 <h6>{text}</h6>
                             </div>
                         </main>
