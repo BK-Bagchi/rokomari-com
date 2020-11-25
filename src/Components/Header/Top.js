52ae9eba92e94d54d0d10d885144194f0d475faa
BK-Bagchi <bkbagchi.dipto@gmail.com>
2020-11-25 10:37:33 +0600

    Header section more markup done

--- src/Components/Header/Top.js
+++ src/Components/Header/Top.js
@@ -2,13 +2,20 @@ import React from 'react'
 import Logo from '../../Images/Logo.png'
 import Cart from '../../Images/Cart.png'
 import User from '../../Images/User.png'
+import Search from '../../Images/Search.png'
 
 const Top = () => {
     return (
-        <section className="top container p-0 d-flex justify-content-between">
+        <section className="top container py-2 d-flex justify-content-between align-items-center">
             <img src={Logo} alt="Rokomari Logo" />
-            <div className="search-box">
-                <input type="text" placeholder="Search by book name" />
+            <div className="search-box-holder">
+                <form className="d-flex w-100">
+                    <p className="m-0 items d-flex align-items-center">Books</p>
+                    <input type="text" placeholder="Search by book name" className="search-box" />
+                    <button className="search-btn">
+                        <img src={Search} alt="Search Icon" />
+                    </button>
+                </form>
             </div>
             <div className="extras">
                 <img src={Cart} alt="Cart" className="mx-2" />
