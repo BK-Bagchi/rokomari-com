fbc68091ff2dd9702ec407d18ab091011ae20a10
BK-Bagchi <bkbagchi.dipto@gmail.com>
2020-11-25 09:15:01 +0600

    Front section more markup done

--- src/Components/FrontView/Front.js
+++ src/Components/FrontView/Front.js
@@ -7,13 +7,13 @@ const Front = () => {
         <section className="front-section">
             <main className="container">
                 <div className="row m-0 w-100">
-                    <div className="col-md-6 d-flex flex-column justify-content-center">
+                    <div className="left col-md-6 p-5 d-flex flex-column justify-content-center">
                         <h6>Are you ready to learn?</h6>
                         <h1>Start learning new things Daily</h1>
                         <button>Let's Start</button>
                     </div>
-                    <div className="col-md-6">
-                        <img src={FrontImg} alt="Front Page Img" />
+                    <div className="right col-md-6 p-5">
+                        <img className="w-100" src={FrontImg} alt="Front Page Img" />
                     </div>
                 </div>
             </main>
